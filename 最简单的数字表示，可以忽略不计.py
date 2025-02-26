6d239d6dd7a06ed8eb232a7ebaf2842c58b2d5a2
EllarDomme <3468433503@qq.com>
2025-02-26 16:06:27 +0800

    Add files via upload

---
+++ 最简单的数字表示，可以忽略不计.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+编写python程序计算下面各表达式的值
+"""
+from math import *
+print(sqrt(pi**2 + 3))
+
+print(log((2*pi*sqrt(13 + e))))
+
+print(pow(tan(log(pi+1,3)),-1))
